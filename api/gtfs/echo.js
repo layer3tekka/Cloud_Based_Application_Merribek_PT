0d673cbb446a2a204e89d772f075e5111ed13601
Callum Hampson-Clarke <161496970+layer3tekka@users.noreply.github.com>
2025-10-10 23:55:51 +1100

    Delete api/gtfs/echo.js

--- api/gtfs/echo.js
+++
@@ -1,7 +0,0 @@
-export default (req, res) => {
-  const mode =
-    req.query?.mode ||
-    new URL(req.url, `http://${req.headers.host}`).pathname.split("/")[3];
-  res.status(200).json({ ok: true, mode });
-};
-
